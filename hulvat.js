4c98806a6692b4318341da2bff1ef6b734a540f6
Sebastian <sebastian.kucera@icloud.com>
2023-09-23 18:29:08 +0200

    hulvat calculator

--- hulvat.js
+++ hulvat.js
@@ -1,15 +1,30 @@
-let hulvat8 = 50;
-let hulva11 = 60;
-let hulvatPolo11 = 60;
-let hulvat12 = 70;
-let hulvatViden12 = 70;
-let hulvat13 = 70;
-let totalValue = 10;
-
-let getHulvat8 = document.getElementById("hulvat8")
 
 let summ = document.getElementById("summ")
+let getReturn = document.getElementById("return")
+let input = document.getElementById("moneyId")
 
-function calculate() {
-    summ.innerHTML = totalValue;
+let summNumber = 0
+let inputNumber = 0
+
+function addValue(buttonElement) {
+    const btnValue = Number(buttonElement.value);
+    summNumber += btnValue;
+    summ.innerText = summNumber;
+    console.log("Added value to summ:", btnValue);
+}
+
+function addMoneyValue(buttonElement) {
+    let currentValue = parseInt(input.value, 10) || 0;
+    input.value = currentValue +  Number(buttonElement.value);
+    inputNumber = currentValue +  Number(buttonElement.value);
+}
+
+function returnMoney() {
+     getReturn.innerText = Number(inputNumber - summNumber)
+}
+
+function reset() {
+    getReturn.innerText = 0;
+    input.value = 0;
+    summ.innerText = 0;
 }
