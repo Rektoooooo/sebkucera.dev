16067e39d2b1a69fd00309e59703c02ec8e7d94a
Sebastian <sebastian.kucera@icloud.com>
2023-09-23 16:13:01 +0200

    hulvat

---
+++ hulvat.js
@@ -0,0 +1,15 @@
+let hulvat8 = 50;
+let hulva11 = 60;
+let hulvatPolo11 = 60;
+let hulvat12 = 70;
+let hulvatViden12 = 70;
+let hulvat13 = 70;
+let totalValue = 10;
+
+let getHulvat8 = document.getElementById("hulvat8")
+
+let summ = document.getElementById("summ")
+
+function calculate() {
+    summ.innerHTML = totalValue;
+}
