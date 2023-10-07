318d9c18168736098e830944a5fff9cb89c4c638
Sebastian <sebastian.kucera@icloud.com>
2023-10-07 14:29:50 +0200

    winchallange

---
+++ winchallangemaps.js
@@ -0,0 +1,43 @@
+function changeMirage() {
+const img = document.getElementById('mirage')
+    img.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Green-checkmark.svg/1200px-Green-checkmark.svg.png'
+}
+
+function changeOffice() {
+    const img = document.getElementById('office')
+    img.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Green-checkmark.svg/1200px-Green-checkmark.svg.png'
+}
+
+function changeDust2() {
+    const img = document.getElementById('dust2')
+    img.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Green-checkmark.svg/1200px-Green-checkmark.svg.png'
+}
+
+function changeAnubis() {
+    const img = document.getElementById('anubis')
+    img.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Green-checkmark.svg/1200px-Green-checkmark.svg.png'
+}
+
+function changeNuke() {
+    const img = document.getElementById('nuke')
+    img.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Green-checkmark.svg/1200px-Green-checkmark.svg.png'
+}
+
+function changeInf() {
+    const img = document.getElementById('inf')
+    img.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Green-checkmark.svg/1200px-Green-checkmark.svg.png'
+}
+
+function changeAnciant() {
+    const img = document.getElementById('anciant')
+    img.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Green-checkmark.svg/1200px-Green-checkmark.svg.png'
+}
+function changeVertigo() {
+    const img = document.getElementById('vertigo')
+    img.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Green-checkmark.svg/1200px-Green-checkmark.svg.png'
+}
+
+function changeOverpass() {
+    const img = document.getElementById('overpass')
+    img.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Green-checkmark.svg/1200px-Green-checkmark.svg.png'
+}
