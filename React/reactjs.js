b9c5ef1f88562625be2ecfd3df5a2031a878bf49
Sebastian <sebastian.kucera@icloud.com>
2024-08-20 20:21:08 +0200

    Updated projects

---
+++ React/reactjs.js
@@ -0,0 +1,12 @@
+function First() {
+    return (
+      <>
+      <h1>First page in react ! MUGAAA</h1>
+      <img src={"https://placekitten.com/g/200/300"} alt={"meow"}/>
+          <ul>
+              <li>Java</li>
+              <li>Typescript</li>
+          </ul>
+      </>
+    )
+}
